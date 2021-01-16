a2359e909c520b624a46c8e8f3f69224a36502f3
Tahmidul <tahmidstat@gmail.com>
2021-01-16 14:43:43 -0500

    upload package

---
+++ R/experiment_data.R
@@ -0,0 +1,20 @@
+#' Experiment data
+#'
+#' @docType data
+#'
+#' @usage data(experiment_data)
+#'
+#' @format An object of class `"tibble"`
+#'
+#' @keywords datasets
+#'
+#' @references Supplied with the assignment by Gradient Metric
+#'
+#'
+#' @source Gradient Metric
+#'
+#' @examples
+#' data(experiment_data)
+#' head(experiment_data)
+#'
+"experiment_data"
---
+++ R/survey_data.R
@@ -0,0 +1,20 @@
+#' Survey data
+#'
+#' @docType data
+#'
+#' @usage data(survey_data)
+#'
+#' @format An object of class `"tibble"`
+#'
+#' @keywords datasets
+#'
+#' @references Supplied with the assignment by Gradient Metric
+#'
+#'
+#' @source Gradient Metric
+#'
+#' @examples
+#' data(survey_data)
+#' head(survey_data)
+#'
+"survey_data"
